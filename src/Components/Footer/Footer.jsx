cab68c7386269a3f41c0d7d4cc5d34f352191968
Muhammad Hassan Ghauri <hassanghauri14@gmail.com>
2024-09-19 15:05:23 +0500

    second commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,32 +1,47 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext } from 'react'
-import './Footer.css'
+import React, { useContext } from "react";
+import "./Footer.css";
 import { MdEmail } from "react-icons/md";
 import { MdSmartphone } from "react-icons/md";
 import { IoLocationSharp } from "react-icons/io5";
 import { RiContactsBook2Fill } from "react-icons/ri";
 import { IoIosArrowUp } from "react-icons/io";
-import { PortfolioContext } from '../../Context/PortfolioContext';
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { PortfolioContext } from "../../Context/PortfolioContext";
 const Footer = () => {
-  const {Contactref,Homeref} =useContext(PortfolioContext);
+  const { Contactref, Homeref } = useContext(PortfolioContext);
   return (
-    <div ref={Contactref} className='footer' id='contact'>
-        <h1>Contact</h1>
-        <hr />
-        <div className='contact-detail'>
-            <p><RiContactsBook2Fill/> Contact me</p>
-            <p><MdEmail/> hassanghauri14@gmail.com</p>
-            <p><MdSmartphone/> 03349505644</p>
-            <p><IoLocationSharp/> Karachi, Pakistan</p>
-        </div>
-        <div className='arrow' onClick={()=>{
-            Homeref.current.scrollIntoView({
-              behavior:"smooth"
-            })
-          }}>
-            <span><IoIosArrowUp/></span>
-        </div>
-        <div className="footer-spans">
+    <div ref={Contactref} className="footer" id="contact">
+      <h1>Contact</h1>
+      <hr />
+      <div className="contact-detail">
+        <p>
+          <RiContactsBook2Fill /> Contact me
+        </p>
+        <p>
+          <MdEmail /> hassanghauri14@gmail.com
+        </p>
+        <p>
+          <MdSmartphone /> 03349505644
+        </p>
+        <p>
+          <IoLocationSharp /> Karachi, Pakistan
+        </p>
+      </div>
+      <div
+        className="arrow"
+        onClick={() => {
+          Homeref.current.scrollIntoView({
+            behavior: "smooth",
+          });
+        }}
+      >
+        <span>
+          <IoIosArrowUp />
+        </span>
+      </div>
+      <div className="footer-spans">
         <span className="footer-150"></span>
         <span className="footer-50"></span>
         <span className="footer-51"></span>
@@ -38,8 +53,19 @@ const Footer = () => {
         <span className="footer-53"></span>
         <span className="footer-102"></span>
       </div>
+      <div className="profiles">
+        <p>View my Profiles on:</p>
+        <div>
+          <a href="https://github.com/HassanGhauri"><span>
+            <FaGithub />
+          </span></a>
+          <a href="https://www.linkedin.com/in/muhammad-hassan-ghauri-80090b247"><span>
+            <FaLinkedin />
+          </span></a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/Projectsview/Projectsview.jsx
+++ src/Components/Projectsview/Projectsview.jsx
@@ -2,14 +2,15 @@
 import React, { useContext } from "react";
 import "./Projectsview.css";
 import { FaPlay } from "react-icons/fa6";
-import video1 from "../../assets/AttendInnApp_Complete_Edit.mp4";
-import video2 from "../../assets/E_commerce_Complete.mp4";
-import video3 from "../../assets/HRMS_Complete_Edit.mp4";
-import img1 from '../../assets/QuizApp.png';
-import img2 from '../../assets/WeatherApp.png';
+import img01 from "../../assets/ATTENDENCEAPP.png";
+import img02 from "../../assets/Ecommerce.png";
+import img03 from "../../assets/HRMS1.png";
+import img04 from "../../assets/ZIPFILEEXTRACTOR.png";
+import img1 from "../../assets/QuizApp.png";
+import img2 from "../../assets/WeatherApp.png";
 import { PortfolioContext } from "../../Context/PortfolioContext";
 const Projectsview = () => {
-  const {Projectref} =useContext(PortfolioContext);
+  const { Projectref } = useContext(PortfolioContext);
   return (
     <div ref={Projectref} className="projects" id="projects">
       <div className="project-intro">
@@ -24,14 +25,10 @@ const Projectsview = () => {
       </div>
       <div className="project-content">
         <div className="details">
-          <video
-            src={video1}
-            style={{ width: "320px", height: "320px" }}
-            controls
-          />
+          <img src={img01} style={{ width: "280px", height: "180px" }} />
           <p>Attendence App</p>
           <p>
-            Github Link:{" "} <br />
+            Github Link: <br />
             <a href="https://github.com/HassanGhauri/Attendence-App">
               https://github.com/HassanGhauri/Attendence-App
             </a>
@@ -39,25 +36,17 @@ const Projectsview = () => {
         </div>
 
         <div className="details">
-          <video
-            src={video2}
-            style={{ width: "320px", height: "320px" }}
-            controls
-          />
+          <img src={img02} style={{ width: "280px", height: "180px" }} />
           <p>E-commerce Website</p>
           <p>
-            Github Link:{" "} <br />
+            Github Link: <br />
             <a href="https://github.com/HassanGhauri/Ecommerce_Website">
               https://github.com/HassanGhauri/Ecommerce_Website
             </a>
           </p>
         </div>
         <div className="details">
-          <video
-            src={video3}
-            style={{ width: "320px", height: "320px" }}
-            controls
-          />
+          <img src={img03} style={{ width: "280px", height: "180px" }} />
           <p>HR managment system </p>
           <p>
             Github Link:{" "}
@@ -67,28 +56,38 @@ const Projectsview = () => {
           </p>
         </div>
         <div className="details">
-          <a href=""><img
-            src={img1}
-            style={{ width: "320px", height: "220px" }}
-          /></a>
+          <a href="">
+            <img src={img1} style={{ width: "280px", height: "180px" }} />
+          </a>
           <p>Quiz App </p>
           <p>
             Github Link:{" "}
             <a href="https://github.com/HassanGhauri/React-Quiz-App">
-            https://github.com/HassanGhauri/React-Quiz-App
+              https://github.com/HassanGhauri/React-Quiz-App
             </a>
           </p>
         </div>
         <div className="details">
-          <a href=""><img
-            src={img2}
-            style={{ width: "320px", height: "220px" }}
-          /></a>
+          <a href="">
+            <img src={img2} style={{ width: "280px", height: "180px" }} />
+          </a>
           <p>Weather App </p>
           <p>
             Github Link:{" "}
             <a href="https://github.com/HassanGhauri/React-Weather-App">
-            https://github.com/HassanGhauri/React-Weather-App
+              https://github.com/HassanGhauri/React-Weather-App
+            </a>
+          </p>
+        </div>
+        <div className="details">
+          <a href="">
+            <img src={img04} style={{ width: "280px", height: "180px" }} />
+          </a>
+          <p>ZipFile Extractor </p>
+          <p>
+            Github Link:{" "}
+            <a href="https://github.com/HassanGhauri/ZipFileExtractor">
+              https://github.com/HassanGhauri/ZipFileExtractor
             </a>
           </p>
         </div>
